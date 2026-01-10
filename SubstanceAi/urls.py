cd0939f3d066a972099a3d582f01b9fd8f05a9e0
toubaabdellahi <21003@supnum.mr>
2026-01-10 17:45:52 +0000

    settings update

--- SubstanceAi/urls.py
+++ SubstanceAi/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from api.views import AskAPIView
 from authapp import urls
 from profilapp import urls
 from .views import *
